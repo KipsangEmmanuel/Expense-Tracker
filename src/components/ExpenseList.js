73906b4150316a27797eb83a1b36f033aa0d55d5
emmqnuelkipsqng <emmqnuelkipsqng@gmail.com>
2023-02-17 14:26:15 +0300

    anner the card

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -2,10 +2,11 @@ import React from 'react'
 import ExpenseDate from './ExpenseDate'
 import './style/ExpenseDate.css'
 
+
 const ExpenseList = (props) => {
     console.log(props.data)
     const listExpense = props.data.map((expenseitem) => (
-        <div class="list-group-item" key={expenseitem.id}>   
+        <div id="list-group-item" class="list-group-item" key={expenseitem.id}>   
             <h5>Expense Item: <span><i>{expenseitem.name}</i></span></h5>
             <h6>Amount: <span><i>{expenseitem.amount}</i></span></h6>
             <h6>Date: <span><ExpenseDate date={expenseitem.date} /></span></h6>
@@ -14,11 +15,11 @@ const ExpenseList = (props) => {
 
   return (
     <div>
-        <div class="card">
+        <div id="card"class="card">
             <div class="card-header">
                 Expense Items
             </div>
-            <ul class="list-group list-group-flush">
+            <ul id="list-group-flush" class="list-group list-group-flush">
                 <li>{listExpense}</li>
                
             </ul>

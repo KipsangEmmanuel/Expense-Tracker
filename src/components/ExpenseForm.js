3e133f868672000363293d9fbe99ddc805fb6016
emmqnuelkipsqng <emmqnuelkipsqng@gmail.com>
2023-02-17 13:25:02 +0300

    create an expense tracker

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,32 +1,80 @@
-import React from 'react'
-import "./style/ExpenseForm.css"
+import React, {useState} from 'react'
+import './style/ExpenseForm.css'
+
+const ExpenseForm = (props) => {
+    const[enteredname, setEnteredName] = useState('')
+    const[enteredamount, setEnteredAmount] = useState('')
+    const[entereddate, setEnteredDate] = useState('')
+
+    const nameChangeHandler = (event) => {
+        setEnteredName(event.target.value)
+    }
+
+    const amountChangeHandler = (e) => {
+        setEnteredAmount(e.target.value)
+    }
+
+    const dateChangeHandler = (e) => {
+        setEnteredDate(e.target.value)
+    }
+
+    const onSubmitHandler = (e) => {
+        e.preventDefault()
+
+        const expenseData = {
+            name: enteredname,
+            amount: enteredamount,
+            date: new Date(entereddate)
+        };
+        props.onNewExpense(expenseData)
+        setEnteredAmount('')
+        setEnteredName('')
+        setEnteredDate('')
 
 
-const ExpenseForm = () => {
+    }
+
   return (
-    <>
-    <form id="main-form">
-        <form id="form-1">
-           <h1 id="add">Add Expense.</h1>
-            <input type="text" id="expense"/><br />
-
+    <div id="expense-form">
+        <h1 id="expense-header">Add Expense</h1>
+        <form onSubmit={onSubmitHandler} class="submit-form">
+            <div class="form-group">
+                <label for="exampleInputexpense">Expense Name</label>
+                <input type="text" 
+                    class="form-control" 
+                    id="exampleInputExpense"
+                    value={enteredname}
+                    onChange={nameChangeHandler}
+                />
+            </div>
+            <div class="form-group">
+                <label for="exampleInputDate">Date</label>
+                <input type="date" 
+                    class="form-control"
+                    onChange={dateChangeHandler}
+                    value={entereddate}
+                ></input>
+            </div>
+            <div class="form-group">
+                <label for="exampleInputexpense">Expense Amount</label>
+                <div class="input-group mb-3">   
+                <div class="input-group-prepend">
+                    <span class="input-group-text">Kshs</span>
+                </div>
+                <input type="number" 
+                    class="form-control" 
+                    aria-label="Amount (to the nearest dollar)" 
+                    onChange={amountChangeHandler}
+                    value={enteredamount}
+                />
+                <div class="input-group-append">
+                    <span class="input-group-text">.00</span>
+                </div>
+                </div>
+            </div>
+            <button id="form-button" type="submit" class="btn btn-primary">Add expense</button>
         </form>
-        <div class="input-group">
-            <input type="text" id="amount" class="form-control" aria-label="Dollar amount (with dot and two decimal places)" />
-            <span class="input-group-text">Ksh</span>
-            <span class="input-group-text">0.00</span>
-        </div>
-
-        <form id="date">
-            <label for="Date">Date</label>
-            <input type="date" name="date" />
-        </form>
-        <div class="submit-btn">
-            <input type="button" class="btn btn-primary" onclick="alert('Hello World!')" value="Submit" />
-
-        </div>
-    </form>
-   </>
+    </div>
   )
 }
 
